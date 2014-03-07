d71d2f6ab37c58c9f1ecc3fab376eea058a1c05e
Valentin Haenel <valentin.haenel@gmx.de>
2014-03-07 14:30:52 +0100

    pep8 the tests

--- test/YamlServerRequestHandler_test.py
+++ test/YamlServerRequestHandler_test.py
@@ -7,7 +7,7 @@ import mock
 
 class YamlServerRequestHandler_tests(TestCase):
 
-    TEST_YAML ="""my super test yaml"""
+    TEST_YAML = """my super test yaml"""
 
     TEST_LOCATIONS = """
         locations:
@@ -33,7 +33,7 @@ class YamlServerRequestHandler_tests(TestCase):
         self.patcher.stop()
 
     def test_get_locations(self):
-        self.request_handler.path ="/"
+        self.request_handler.path = "/"
         self.request_handler.do_GET()
 
         self.request_handler.send_header.assert_any_call('ETag', 'e363ed10bccdb2842e14498ef956a346a83f0617ddeeb8e991a654691c0ea836')
@@ -42,7 +42,7 @@ class YamlServerRequestHandler_tests(TestCase):
         self.request_handler.wfile.write.assert_any_call(self.TEST_LOCATIONS)
 
     def test_get_yaml(self):
-        self.request_handler.path ="/unittest"
+        self.request_handler.path = "/unittest"
         self.request_handler.do_GET()
 
         self.request_handler.send_header.assert_any_call('ETag', '6ee9c999bead9a4015de1b5108e3ac005a4893aa6e79e207b3be403063f1a296')
